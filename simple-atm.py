3e5a0d90b2b8d40c18a355c7ff123cc1e0f98e07
Csi18nAlistairMann <Csi18nAlistairMann@users.noreply.github.com>
2021-05-13 19:00:16 +0100

    Update simple-atm.py

--- simple-atm.py
+++ simple-atm.py
@@ -62,218 +62,6 @@
 # in the setter without needing to check anything anywhere else.
 #
 
-# ---------------------------------------------
-# Section 1: constants
-#
-# A constant is a variable who's value does not change throughout the life of
-# a run. Some constants are built in (True, False); messages sent to the user
-# are usually constants, and there's often others. By defining the constants
-# here someone can later improve things (such as my bad French) without needing
-# access to the code itself. For instance, this can help limit the damage a
-# mischievous translator could do.
-#  A second reason is to reduce errors. For instance I used CONST_YES instead
-# of "yes" in the code. That means I can't accidentally use "Yes" instead and
-# so introduce a potential bug as "yes" != "Yes"
-#
-# Note that Python doesn't have constants so we use variables with uppercase
-# names to indicate to ourselves we shouldn't be changing their contents.
-
-#
-# Language Constants
-# English
-CONST_EN = 'en'
-MSG_EN_YOUR_BALANCE_WAS = "Your balance was "
-MSG_EN_YOUR_BALANCE_IS = "Your balance is now "
-# Note parenthesis used to keep lines under 80 chars long
-MSG_EN_HOW_MUCH = (
-    "Please enter how much you would like to deposit or withdraw ")
-MSG_EN_WELCOME = "Welcome"
-MSG_EN_INVALID_INPUT_TRY_AGAIN = "Invalid response, please try again"
-MSG_EN_CONTINUE_YN = "Would you like to continue? yes/no "
-# French
-CONST_FR = 'fr'
-MSG_FR_YOUR_BALANCE_WAS = "Votre solde était "
-MSG_FR_YOUR_BALANCE_IS = "Votre solde est maintenant "
-MSG_FR_HOW_MUCH = (
-    "Veuillez saisir le montant que vous souhaitez déposer ou retirer ")
-MSG_FR_WELCOME = "Bienvenue"
-MSG_FR_INVALID_INPUT_TRY_AGAIN = "Réponse non valide, veuillez réessayer"
-MSG_FR_CONTINUE_YN = "Voulez-vous continuer? Y (oui)/N (Non) "
-
-#
-# Other constants
-CONST_WAS = "was"
-CONST_IS = "is"
-CONST_YES = "yes"
-CONST_NO = "no"
-CONST_OTHER = "other"
-CONST_INITIAL_BALANCE = 100
-
-# ---------------------------------------------
-# Section 2: classes
-#
-# The class models some object in the real world. Your ATM code refers to two
-# such objects: one is the ATM hardware with its screen and keyboard, and one
-# is the account which is being adjusted for the user. We know they are
-# different objects because its not normal for a bank account to assume you
-# have a keyboard, and likewise wrong for a screen to assume you want to
-# adjust money amounts through it.
-
-# The ATM hardware assumes a screen to which it can send output, a keyboard
-# from which it can receive input, and a language set up when its first
-# used. The language right now is either French or English and assists in
-# abstracting out the message being sent to the user from the content of
-# that message. A later improvement might see Portuguese added, or a braille
-# output added. In fact we could implement sending output to a printer
-# instead of a screen, or have them ATM interact via email, etc.
-class Bank():
-    bankLanguage = ''
-    bankAccount = ''
-    atmMachine = ''
-
-    def __init__(self, language):
-        self.bankLanguage = language
-        self.atmMachine = atmHardware(language)
-        self.bankAccount = atmAccount()
-
-    # Conduct a fake transaction
-    def businessProcess001FakeATMTransaction(self):
-        # show the balance
-        bank.atmMachine.showBalanceToScreen(CONST_WAS,
-                                            self.bankAccount.getBalance())
-        bank.atmMachine.showNewLine()
-        # get the amount to adjust from user and apply it
-        wd = bank.atmMachine.obtainAmountFromKeyboard()
-        self.bankAccount.applyTransaction(wd)
-        # show the new balance
-        bank.atmMachine.showNewLine()
-        bank.atmMachine.showBalanceToScreen(CONST_IS,
-                                            self.bankAccount.getBalance())
-        bank.atmMachine.showNewLine()
-
-    # Get a yes or no from the user: anything else? Ask again
-    def businessProcess002CheckDoAgain(self):
-        reply = CONST_OTHER
-        while (reply == CONST_OTHER):
-            reply = bank.atmMachine.showContinueYesOrNo()
-            bank.atmMachine.showNewLine()
-            if reply == CONST_YES:
-                bank.atmMachine.printToScreen(bank.bankAccount.getBalance())
-                reply = CONST_YES
-            elif reply == CONST_NO:
-                bank.atmMachine.printToScreen("test2")
-            else:
-                bank.atmMachine.showInvalidTryAgain()
-                bank.atmMachine.showNewLine()
-                bank.atmMachine.showNewLine()
-                reply = CONST_OTHER
-        return reply
-
-# The ATM is an object of its own too. It doesn't really care about what the
-# user means or expects to happen, it just cares about displaying messages to
-# that user (Output) and receiving direction from that user (Input).
-#
-# I add 'language' to this object as by abstracting it out we can more easily
-# make the ATM useful to foreign language speakers, perhaps improving sales
-class atmHardware():
-    language = ''
-
-    # Initialising an ATM must include the language expected
-    def __init__(self, lang):
-        self.setLanguage(lang)
-
-    def setLanguage(self, lang):
-        self.language = lang
-
-    def getLanguage(self):
-        return self.language
-
-    # Methods common to input and output
-    def printToScreen(self, msg):
-        print(msg)
-
-    # -- Output methods --
-    # By putting each kind of interaction into its own method we can leave room
-    # for changes later: changes to language, changes to output method and so
-    # forth. If the ATM had a screen output, and a Braille output, we might put
-    # them in their own classes.
-    def showBalanceToScreen(self, tense, balance):
-        if self.getLanguage() == CONST_FR:
-            if tense == CONST_WAS:
-                msg = MSG_FR_YOUR_BALANCE_WAS + str(balance)
-            else:
-                msg = MSG_FR_YOUR_BALANCE_IS + str(balance)
-        else:
-            if tense == CONST_WAS:
-                msg = MSG_EN_YOUR_BALANCE_WAS + str(balance)
-            else:
-                msg = MSG_EN_YOUR_BALANCE_IS + str(balance)
-        self.printToScreen(msg)
-
-    def showNewLine(self):
-        msg = " "
-        self.printToScreen(msg)
-
-    def showWelcome(self):
-        if self.getLanguage() == CONST_FR:
-            msg = MSG_FR_WELCOME
-        else:
-            msg = MSG_EN_WELCOME
-        self.printToScreen(msg)
-
-    def showInvalidTryAgain(self):
-        if self.getLanguage() == CONST_FR:
-            msg = MSG_FR_INVALID_INPUT_TRY_AGAIN
-        else:
-            msg = MSG_EN_INVALID_INPUT_TRY_AGAIN
-        self.printToScreen(msg)
-
-    # -- Input methods --
-    # Again I'm keeping code with similar tasks together. One thing important
-    # with input is we want to make sure it's sanitised - we don't want user's
-    # able to screw the code up. We barely santise code here.
-
-    # No sanitising here: no decimals accepted because of int, which will
-    # crash if we don't receive a number at all
-    def obtainAmountFromKeyboard(self):
-        if self.getLanguage() == CONST_FR:
-            msg = MSG_FR_HOW_MUCH
-        else:
-            msg = MSG_EN_HOW_MUCH
-        return int(input(msg))
-
-    # Sanitising of a sort done here: we only accept the first character typed
-    # in, we force that character to lower, and we return not that character
-    # but a constant that we choose
-    def showContinueYesOrNo(self):
-        if self.getLanguage() == CONST_FR:
-            msg = MSG_FR_CONTINUE_YN
-        else:
-            msg = MSG_EN_CONTINUE_YN
-        response = input(msg)
-        lcFirstLetter = response[0].lower()
-        if lcFirstLetter == "y":
-            return CONST_YES
-        elif lcFirstLetter == "n":
-            return CONST_NO
-        return CONST_OTHER
-
-# The ATM Account is an object of its own kind: it can report what the balance
-# is, adjust itself relatively or absolutely, and can do so either manually
-# where the account must interact with a user thought to be physically present
-# or be prepared to work automatically - say in response to a standing order.
-class atmAccount():
-    def setBalance(self, balance):
-        self.balance = balance
-
-    def getBalance(self):
-        return self.balance
-
-    def adjustBalance(self, wd):
-        self.setBalance(self.getBalance() - wd)
-
-    def applyTransaction(self, wd):
-        self.adjustBalance(wd)
 
 # ---------------------------------------------
 # Section 3: Set up
@@ -282,17 +70,23 @@ class atmAccount():
 # real stuff done: loading data, preparing variables and so forth.
 
 #
+# Imports
+from classes import *
+from constants import *
+
+#
 # Defaults
-startBalance = CONST_INITIAL_BALANCE
 
 #
 # Start up
 bank = Bank(CONST_EN)
-bank.bankAccount.setBalance(startBalance)
+bank.bankAccount.setBalance(CONST_INITIAL_BALANCE)
+# Show welcome and check PIN
 bank.atmMachine.showNewLine()
 bank.atmMachine.showNewLine()
 bank.atmMachine.showWelcome()
 bank.atmMachine.showNewLine()
+bank.businessProcess003CheckPin()
 
 # ---------------------------------------------
 # Section 4: Main loop
